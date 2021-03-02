e3a78bcb0d2522b87b7b39a695de850909d129ef
estherndichu <itskuijenga@gmail.com>
2021-03-02 23:59:52 +0300

    initialize flask login

--- manage.py
+++ manage.py
@@ -2,14 +2,20 @@ from app import create_app,db
 from flask_script import Manager,Shell, Server
 from app.models import User,Pitches
 from flask_migrate import Migrate,MigrateCommand
+from flask_login import LoginManager
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
 migrate = Migrate(app,db)
+
+manager.add_command('server',Server)
 manager.add_command('db',MigrateCommand)
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 @manager.shell
 def make_shell_context():

d28776e7b25ab652c7fa991c168f733f32790b1b
estherndichu <itskuijenga@gmail.com>
2021-03-02 18:57:35 +0300

    new app structure and added extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #initializing app
 app = Flask(__name__)
+
+#initializing Flask extensions
+bootstrap = Bootstrap(app)
 
 #setting up configuration
 app.config.from_object(DevConfig)
--- app/views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.route('/')
-def index():
-    '''
-    View root page that returns the index page and its data
-    '''
-    return render_template('index.html')

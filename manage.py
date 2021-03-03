bc5ce74bba71264893ecc247721c216dcdf543ec
estherndichu <itskuijenga@gmail.com>
2021-03-03 12:34:37 +0300

    migrations and edit manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from flask_script import Manager,Shell, Server
+from flask_script import Manager,Server
 from app.models import User,Pitches
 from flask_migrate import Migrate,MigrateCommand
 
@@ -7,13 +7,14 @@ from flask_migrate import Migrate,MigrateCommand
 app = create_app('production')
 
 manager = Manager(app)
-migrate = Migrate(app,db)
-
 manager.add_command('server',Server)
-manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User, Pitches = Pitches)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 if __name__ == '__main__':
     manager.run()
